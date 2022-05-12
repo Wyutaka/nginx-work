d3fea883f89ccee10c1405b9218abbfa7ceb518b
Wyutaka <tihiro.haul@gmail.com>
2022-05-12 18:24:42 +0900

    ログ出力の方法を考察中 アスキーに対応した文字列の配列を作らないとだめ？ ex) ascii['a'] = "63"

--- module/hexdump.c
+++ module/hexdump.c
@@ -1,21 +1,26 @@
+/*!
+ * dumphex.c
+ *
+ * written by www
+ * 
+ * 与えられたテキストのダンプを返す関数の実装.
+ * やること
+ * 文字列ポインタを与え，そのポインタにダンプ情報を書き込む．
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define ARRAY_SIZE (6)
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#define ARRAY_SIZE (6)
-
-void DumpHex(const void* data, size_t size);
+void DumpHex(const void* data,  char *msg, size_t size);
 
 int main(void)
 {
     char *hoges;
     // char str1[ARRAY_SIZE] = "abc";
-    char str2[] = {0x63, 0x61, 0x74, 0x00, 0x75, 0x63, 0x61, 0x74, 0x00, 0x75, 0x63, 0x61, 0x74, 0x00, 0x75, 0x63, 0x61, 0x74, 0x00, 0x75};
+    char str2[] = {0x63, 0x61, 0x74, 0x00, 0x75, 0x63, 0x61, 0x74, 0x00, 0x75, 0x63, 0x61, 0x74, 0x00, 0x75, 0x63, 0x61, 0x74, 0x00 };
     
     hoges = (char *)calloc(ARRAY_SIZE, sizeof(char));
 
@@ -31,14 +36,27 @@ int main(void)
     hoges[k] = 0x00;
     char e[16];
     char *hoges2 = str2;
+    char *dmsg;
+
+    dmsg = (char *)calloc(sizeof(str2), sizeof(char));
     
-    DumpHex(hoges2, sizeof(str2));
+    DumpHex(hoges2, dmsg, sizeof(str2));
+
+    printf("dmsg is : %s", dmsg);
     
 }
 
-
-void DumpHex(const void* data, size_t size) {
+void DumpHex(const void* data, char *dmsg, size_t size) {
+    char ascii[129][2];
+    ascii['a'][0] = '6';
+    ascii['a'][1] = '3';
+    ascii['b'][0] = '6';
+    ascii['b'][1] = '4';
 	char *rdmsg =  (char *)calloc(size, sizeof(char));
+
+    printf("test %s\n", ascii['a']);
+    strncpy(dmsg, ascii['a'], 2);
+    
 	const int ol = 16;
 	size_t i, j;
 	for (i = 0; i < size; ++i) { 
